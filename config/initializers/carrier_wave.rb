48924d4fe2d7e96830a885cad6ecfd6036d9fde7
Kyle Kravette <kkravette@gmail.com>
2015-07-25 01:05:43 -0400

    trying to fix s3

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,15 @@
 if Rails.env.production?
 	CarrierWave.configure do |config|
-		config.fog_credentials = {
-			#configureation for amazon s3
-			:provider => 'AWS',
-			:aws_access_key_id => ENV['S3_ACCESS_KEY'],
-			:aws_secret_access_key => ENV['S3_SECRET_KEY']
-		}
-		config.fog_directory => ENV['S3_BUCKET']
+	  config.fog_provider = 'fog/aws'                        # required
+	  config.fog_credentials = {
+	    provider:              'AWS',
+	    aws_access_key_id:     ENV['S3_ACCESS_KEY'],                        
+	    aws_secret_access_key: ENV['S3_SECRET_KEY'],                        
+	  }
+	  config.fog_directory  = ENV['S3_BUCKET']
 	end
 end
+
+#ENV['S3_ACCESS_KEY']
+#ENV['S3_SECRET_KEY']
+#ENV['S3_BUCKET']
